50f3d7a81328426bac724a14b87bdab51efecc75
LiamH <liamhearts@gmail.com>
2019-04-17 12:13:39 -0700

    everything is working besides pause/play?

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,59 @@
 //TODO when a button is clicked search giphy API for "this" name
+
+
+
 //TODO search bar will add a button to the page
+
+
+//TODO generate buttons on load-up
+var buttonArray = ["tree", "mountain", "utah", "beach"];
+
+function buttonLoad() {
+    $("#btnHangout").empty()
+    for (let i = 0; i < buttonArray.length; i++) {
+        console.log(buttonArray[i])
+        var gifButton = $("<button>")
+        gifButton.addClass("gifButtons btn btn-info")
+
+        gifButton.attr("data-name", buttonArray[i])
+        gifButton.text(buttonArray[i])
+        $("#btnHangout").append(gifButton)
+    }
+}
+
+buttonLoad()
+
+
+$("#searchBtn").on("click", function () {
+    var btnName = $("#gifSearch").val()
+    buttonArray.push(btnName)
+    buttonLoad()
+})
+
+
+$(document).on("click", ".gifButtons", function () {
+    console.log("working here")
+    var gifName = $(this).attr("data-name")
+    console.log(gifName)
+    var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=zzluTfZGQ8bBJajVD4ZGmDiZljA9XMoO&q=" + gifName + "&limit=10&offset=0&&lang=en"
+
+    $("#gifDiv").empty()
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (response) {
+            var results = response.data
+            console.log(results)
+            for (let i = 0; i < results.length; i++) {
+                var gif = $("<img>")
+                gif.addClass("gif")
+                gif.attr("src", results[i].images.fixed_height_still.url)
+                $("#gifDiv").append(gif)
+            }
+        })
+
+
+
+})
+
